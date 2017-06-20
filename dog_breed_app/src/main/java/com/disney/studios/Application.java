11ec6a7c40c898b634e07b8a90ae1fa8d46340b9
Suresh Adhikari <mesuraceer@gmail.com>
2017-06-19 19:38:27 -0700

    Refactor main application class

--- dog_breed_app/src/main/java/com/disney/studios/Application.java
+++ dog_breed_app/src/main/java/com/disney/studios/Application.java
@@ -1,5 +1,9 @@
 package com.disney.studios;
 
+/**
+ * @author suresh adhikari
+ *
+ */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
